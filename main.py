d7d4b40232f2268293b6cda4840c5cdb8461e258
TsubasaOmi <t.omi@benjamin.co.jp>
2024-01-14 20:32:32 +0900

    added send button again to improve senidng the prompt correctly and added click the text box one more and exception handling for load config

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter
 import pyautogui
 import time
@@ -19,8 +20,23 @@ inputBoxListenerThread = None
 inputBoxListenerRunning = False
 inputBoxXCoordinate = 0
 inputBoxYCoordinate = 0
+sendButtonXCoordinate = 0
+sendButtonYCoordinate = 0
 isProcessRunning = False
 automationThread = None
+
+# デフォルト設定値
+DEFAULT_CONFIG = {
+    "input_box": {
+        "x": 0,  # デフォルトの入力欄のX座標
+        "y": 0   # デフォルトの入力欄のY座標
+    },
+    "send_button": {
+        "x": 0,  # デフォルトの送信ボタンのX座標
+        "y": 0   # デフォルトの送信ボタンのY座標
+    },
+    "wait_time": 5  # デフォルトの応答待ち時間（秒）
+}
 
 def on_input_box_coordinates_button_click(x, y, button, pressed):
     global inputBoxListenerRunning
@@ -52,6 +68,36 @@ def input_box_coordinates_start_button():
         inputBoxListenerThread = threading.Thread(target=start_listener_for_input_bot)
         inputBoxListenerThread.start()
 
+def on_send_button_coordinates_button_click(x, y, button, pressed):
+    global inputBoxListenerRunning
+    if not inputBoxListenerRunning:
+        return False
+    if pressed and button == Button.left:
+        # print(f"Mouse clicked at ({x}, {y})")
+        # メインスレッドでラベルを更新する　理由:Tkinterがシングルスレッドで動作するGUIフレームワークであるため
+        # 詳細：pynput ライブラリのリスナーは別スレッドで動作しているため、
+        # 直接GUIを更新することはスレッドの安全性に反します。
+        # root.after を使用することで、スレッド間の競合を避けつつ、メインスレッドにGUIの更新をスケジュールすることができます。
+        root.after(0, lambda: send_button_coordinates_label.config(text=f"X: {x}, Y: {y}"))
+        global sendButtonXCoordinate, sendButtonYCoordinate
+        sendButtonXCoordinate = x
+        sendButtonYCoordinate = y
+    if pressed and button == Button.right:
+        send_button_coordinates_button.config(text='送信ボタン座標取得')
+        inputBoxListenerRunning = False
+
+def start_listener_for_send_button():
+    with Listener(on_click=on_send_button_coordinates_button_click) as listener:
+        listener.join()
+
+def send_button_coordinates_start_button():
+    global inputBoxListenerThread, inputBoxListenerRunning
+    if not inputBoxListenerRunning:
+        send_button_coordinates_button.config(text='クリックして入力欄座標取得してください。右クリックで終了します。')
+        inputBoxListenerRunning = True
+        inputBoxListenerThread = threading.Thread(target=start_listener_for_send_button)
+        inputBoxListenerThread.start()
+
 def start_function():
     try:
         global isProcessRunning
@@ -71,6 +117,8 @@ def start_function():
             if isProcessRunning:
                 # 保存された座標を使用してテキスト入力欄にクリック
                 pyautogui.click(inputBoxXCoordinate, inputBoxYCoordinate)
+                # ChatGPTのウィンドウがアクティブになってない場合のためにもう一度クリック
+                pyautogui.click(inputBoxXCoordinate, inputBoxYCoordinate)
                 print(f"Mouse clicked at ({inputBoxXCoordinate}, {inputBoxYCoordinate})")
                 # ChatGPTの入力欄に「続きを書いてください。」と入力
                 pyautogui.write('Continue generating in Japanese.', interval=0.05)
@@ -81,13 +129,14 @@ def start_function():
                 return
 
             if isProcessRunning:
-                # Enterキーを押して送信
-                pyautogui.press('enter')
-                print("Enterを押しました。")
+                # 送信ボタンの座標を使用してクリック
+                pyautogui.click(sendButtonXCoordinate, sendButtonYCoordinate)
     except ValueError:
         print("無効な待ち時間が入力されました。数値を入力してください。")
+        sys.exit(1)  # プログラムを終了
     except Exception as e:
         traceback.print_exc()  # 例外の詳細を出力
+        sys.exit(1)  # プログラムを終了
 
 def start_thread():
     """スタートボタンのスレッドを開始する関数。"""
@@ -113,35 +162,81 @@ def save_configuration():
             'x': inputBoxXCoordinate,
             'y': inputBoxYCoordinate
         },
+        'send_button': {
+            'x': sendButtonXCoordinate,
+            'y': sendButtonYCoordinate
+        },
         'wait_time': wait_time_entry.get()  # ユーザー入力の待ち時間
     }
     with open(CONFIG_FILENAME, 'w') as config_file:
         json.dump(config_data, config_file, indent=4)
     print("設定を保存しました。")
 
+def create_default_config_file():
+    """デフォルトの設定値で設定ファイルを新規作成する"""
+    print("デフォルト値を使用して新しい設定ファイルを作成します。")
+    with open(CONFIG_FILENAME, 'w') as config_file:
+        json.dump(DEFAULT_CONFIG, config_file, indent=4)
+    return DEFAULT_CONFIG
+
+# 設定ファイルの設定値を更新する関数
+def update_config_data(config_data):
+    # グローバル変数の更新
+    global inputBoxXCoordinate, inputBoxYCoordinate, sendButtonXCoordinate, sendButtonYCoordinate
+
+    try:
+        inputBoxXCoordinate = config_data['input_box']['x']
+        inputBoxYCoordinate = config_data['input_box']['y']
+        input_box_coordinates_label.config(text=f"入力欄：X: {inputBoxXCoordinate}, Y: {inputBoxYCoordinate}")
+    except KeyError:
+        print("設定ファイルに input_box キーが存在しません。")
+        sys.exit(1)  # プログラムを終了
+
+    try:
+        sendButtonXCoordinate = config_data['send_button']['x']
+        sendButtonYCoordinate = config_data['send_button']['y']
+        send_button_coordinates_label.config(text=f"送信ボタン：X: {sendButtonXCoordinate}, Y: {sendButtonYCoordinate}")
+    except KeyError:
+        print("設定ファイルに send_button キーが存在しません。")
+        sys.exit(1)  # プログラムを終了
+    try:
+        wait_time = config_data['wait_time']
+        wait_time_entry.delete(0, tkinter.END)
+        wait_time_entry.insert(0, wait_time)
+    except KeyError:
+        print("設定ファイルに wait_time キーが存在しません。")
+        sys.exit(1)  # プログラムを終了
+
 # 設定を読み込む関数
 def load_configuration():
     try:
         with open(CONFIG_FILENAME, 'r') as config_file:
             config_data = json.load(config_file)
-            global inputBoxXCoordinate, inputBoxYCoordinate
-            inputBoxXCoordinate = config_data['input_box']['x']
-            inputBoxYCoordinate = config_data['input_box']['y']
-            # ラベルを更新する
-            input_box_coordinates_label.config(text=f"入力欄：X: {inputBoxXCoordinate}, Y: {inputBoxYCoordinate}")
-            wait_time_entry.delete(0, tkinter.END)
-            wait_time_entry.insert(0, config_data['wait_time'])
-            print("設定を読み込みました。")
+            update_config_data(config_data)
+        print("設定を読み込みました。")
+
     except FileNotFoundError:
-        print("設定ファイルが見つかりません。デフォルト値を使用します。")
+        print("設定ファイルが見つかりません。デフォルト値を使用して設定ファイルを作成します。")
+        config_data = create_default_config_file()
+        update_config_data(config_data)
+        print("設定を読み込みました。")
     except json.JSONDecodeError:
-        print("設定ファイルが破損しています。デフォルト値を使用します。")
+        print("設定ファイルが破損しています。デフォルト値を使用して設定ファイルを作成します。")
+        config_data = create_default_config_file()
+        update_config_data(config_data)
+        print("設定を読み込みました。")
 
 input_box_coordinates_label = tkinter.Label(root, text="入力欄：X: 0, Y: 0")
 input_box_coordinates_label.pack()
 
 input_box_coordinates_button = tkinter.Button(root, text="メッセージボックス座標取得", command=input_box_coordinates_start_button)
 input_box_coordinates_button.pack()
+
+send_button_coordinates_label = tkinter.Label(root, text="送信ボタン：X: 0, Y: 0")
+send_button_coordinates_label.pack()
+
+send_button_coordinates_button = tkinter.Button(root, text="送信ボタン座標取得", command=send_button_coordinates_start_button)
+send_button_coordinates_button.pack()
 
 # 応答待ち時間入力フィールドとラベルを追加
 wait_time_label = tkinter.Label(root, text="応答待ち時間（秒）:")
